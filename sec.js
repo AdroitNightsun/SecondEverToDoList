e34527ffa956bf5482dbbf5715b575864d246077
Karan Kumar <102454582+AdroitNightsun@users.noreply.github.com>
2025-07-06 23:09:52 +0530

    Refactor code for consistency and readability in sec.js

--- sec.js
+++ sec.js
@@ -1,39 +1,38 @@
 let list = [];
 
-window.addEventListener('load', function(){
-  const SavedList = JSON.parse(localStorage.getItem('ToDoList'));
+window.addEventListener("load", function () {
+  const SavedList = JSON.parse(localStorage.getItem("ToDoList")) || [];
   list = SavedList;
   renderList();
-})
+});
 
-      function addItem() {
-        let input = document.querySelector(".todo-input");
-        let value = input.value.trim();
+function addItem() {
+  let input = document.querySelector(".todo-input");
+  let value = input.value.trim();
 
-        if (value === "") {
-          alert("You can't add empty text");
-          return;
-        }
-        
+  if (value === "") {
+    alert("You can't add empty text");
+    return;
+  }
 
-        list.push(value);
-        renderList();
-        input.value = "";
-        localStorage.setItem('ToDoList', JSON.stringify(list));
-      }
+  list.push(value);
+  renderList();
+  input.value = "";
+  localStorage.setItem("ToDoList", JSON.stringify(list));
+}
 
-      function renderList() {
-        const taskli = document.getElementById("taskli");
-        taskli.innerHTML = list
-          .map(
-            (item, index) =>
-              `<li>${item}<button class="del_btn" onclick="del(${index})">Delete</button></li>`
-          )
-          .join("");
-      }
+function renderList() {
+  const taskli = document.getElementById("taskli");
+  taskli.innerHTML = list
+    .map(
+      (item, index) =>
+        `<li>${item}<button class="del_btn" onclick="del(${index})">Delete</button></li>`
+    )
+    .join("");
+}
 
-      function del(index) {
-        list.splice(index, 1);
-        renderList();
-        localStorage.setItem('ToDoList', JSON.stringify(list));
-      }
+function del(index) {
+  list.splice(index, 1);
+  renderList();
+  localStorage.setItem("ToDoList", JSON.stringify(list));
+}
